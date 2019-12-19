59b8935f40f4ae9003492b64664e714f928b00fd
Alireza Abbasi <AlirezaAbbasiM@gmail.com>
2019-12-19 15:27:40 +0330

    use std::ofstream File

--- Chapter13/Chapter13/Chapter13.cpp
+++ Chapter13/Chapter13/Chapter13.cpp
@@ -7,12 +7,30 @@
 
 int main()
 {
-	std::ofstream File{ "newFile.txt", std::ios::out };
-	if (!File)
-	{
-		std::cerr << "File is not opened!\n";
-		exit(EXIT_FAILURE);
-	}
 	
+	try
+	{
+		std::ofstream File{ "newFile.txt", std::ios::out };
+		if (!File)
+		{
+			std::cerr << "File is not opened!\n";
+			exit(EXIT_FAILURE);
+		}
+
+		std::cout << "Enter Account, name, and balance.\n";
+		int account{};
+		std::string name;
+		double balance{};
+
+		while (std::cin >> account >> name >> balance)
+		{
+			File << account << " " << name << " " << balance << std::endl;
+			std::cout << "?";
+		}
+	}
+	catch (const std::exception&)
+	{
+
+	}
 }
 
